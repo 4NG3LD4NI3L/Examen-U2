476fb5eca7f226d948d28e4a14297c5648dbdf89
4NG3LD4NI3L <aromero_21@alu.uabcs.mx>
2024-09-11 14:33:52 -0700

    first commit

---
+++ home.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('my_canvas');
+const ctx = canvas.getContext('2d');
+
+// Dibujamos un rectángulo como ejemplo
+ctx.strokeStyle = 'black';
+ctx.strokeRect(50, 50, 700, 700);
